96cf23d6f88a5596e8ab0b4edbbb20d39c1ab370
Daniel Roa <danielroapetrasic@gmail.com>
2021-06-21 00:37:06 -0400

    20210621_1

--- server.js
+++ server.js
@@ -80,16 +80,18 @@ var restApi = expressNedbRest();
 restApi.addDatastore('banco', datastore);
 
 //********************************************** */
-// Cross Origin middleware
-oApp.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-  })
+// Configure Header HTTP 
+oApp.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
   //********************************************** */
 
 // setup express server to serve rest service
-oApp.use('./', restApi);
+oApp.use('/', restApi);
 
 oApp.listen(9090, function () {
     console.log('you may use nedb rest api at port 9090');
